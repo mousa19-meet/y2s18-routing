23a154753e20387b51a234469c305e91f6a594cc
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 16:17:17 +0300

    im bored

--- exercises/app.py
+++ exercises/app.py
@@ -2,12 +2,21 @@ from databases import *
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template("home.html")
 
+@app.route('/hello')
+def Heyy(name):
+	return render_template("hello.html",name=name)
+
 @app.route('/student/<int:student_id>')
 def display_student(student_id):
 	return render_template('student.html',student_id=student_id)
+
+#@app.route('/student')
+#def pick_an_id():
+#	return "pick a student"
 
 app.run(debug=True)
--- lab/app.py
+++ lab/app.py
@@ -7,7 +7,7 @@ def home():
     return render_template('home.html')
 
 @app.route('/student/<int:student_id>')
-def display_student(student_id):
-    return render_template('student.html', student_id=student_id)
+def display_student(student_id,student):
+    return render_template('student.html', student_id=student_id,student=query_by_id(student_id))
 
 app.run(debug=True)
